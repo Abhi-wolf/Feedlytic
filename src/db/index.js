7e599a38faa7423e3df56e6aac72479fba9cf736
Abhi-wolf <abhijeetkr2020@outlook.com>
2024-12-18 21:47:26 +0530

    Used pool option in database

--- src/db/index.js
+++ src/db/index.js
@@ -1,27 +1,27 @@
-// import { drizzle } from "drizzle-orm/node-postgres";
-// import { Pool } from "pg";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 
-// let sslMode = "require";
-// if (process.env.NODE_ENV === "development") {
-//   sslMode = "disable";
-// }
+let sslMode = "require";
+if (process.env.NODE_ENV === "development") {
+  sslMode = "disable";
+}
 
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL + "?sslmode=" + sslMode,
-//   max: 20,
-//   idleTimeoutMillis: 30000,
-//   connectionTimeoutMillis: 2000,
-// });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+});
 
-// export const db = drizzle(pool, { logger: false });
-// // export const db = drizzle(pool, { logger: true });
-
-// export * from "drizzle-orm";
-
-import { drizzle } from "drizzle-orm/neon-http";
-import { neon } from "@neondatabase/serverless";
-
-const sql = neon(process.env.DATABASE_URL);
-export const db = drizzle({ client: sql });
+export const db = drizzle(pool, { logger: false });
+// export const db = drizzle(pool, { logger: true });
 
 export * from "drizzle-orm";
+
+// import { drizzle } from "drizzle-orm/neon-http";
+// import { neon } from "@neondatabase/serverless";
+
+// const sql = neon(process.env.DATABASE_URL);
+// export const db = drizzle({ client: sql });
+
+// export * from "drizzle-orm";
